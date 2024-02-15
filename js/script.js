4a961aa79f1a88e36f5e791d7c82dee8f453f10f
Myktybek7 <myktybekabdykaiymov@gmail.com>
2024-02-15 10:35:27 +0600

    web_page-onLesson

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/*const myHeading = document.querySelector("h1");
+
+myHeading.textContent = "Hello, World!";
+alert(myVariable);
+
+myVariable = "Myktybek"*/
+
+const imgElement = document.querySelector("img");
+
+const img1URL = "../img/moon.jpg";
+const img2URL = "../img/pink.jpg";
+
+function changePhoto(event){
+    let current = imgElement.getAttribute("src");
+    if(current == img1URL){
+        imgElement.setAttribute("src", img1URL);
+    }
+    else {
+        imgElement.setAttribute("src", img2URL)
+    }
+}
+
+imgElement.onclick = changePhoto;
